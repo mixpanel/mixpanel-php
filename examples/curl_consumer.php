c9650186c6d08a704b0f27c70715bf51fde3e4c1
jbwyme <wymetyme@gmail.com>
2013-06-20 16:19:48 -0600

    Update curl_consumer.php

--- examples/curl_consumer.php
+++ examples/curl_consumer.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../lib/Mixpanel.php"); // import the Mixpanel class
 
+// Make calls using the PHP cURL extension not using SSL
+// Warning: This will block until the requests are complete.
 $mp = new Mixpanel("MIXPANEL_PROJECT_TOKEN", array(
     "debug"             => true,
     "consumer"          => "curl",
