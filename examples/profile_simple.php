655e04c975450eb72830cb0e8a5453e985a54c42
Anton Zagorskii <anton@lumio-app.com>
2023-11-07 09:54:05 +0000

    PSR-4, Bump minimum PHP version to 7.1, phpunit to ^7.0

--- examples/profile_simple.php
+++ examples/profile_simple.php
@@ -1,5 +1,7 @@
 <?php
-require_once("../lib/Mixpanel.php"); // import the Mixpanel class
+
+use Mixpanel\Mixpanel;
+
 $mp = Mixpanel::getInstance("MIXPANEL_PROJECT_TOKEN"); // instantiate the Mixpanel class
 
 // create or update a profile with First Name, Last Name, E-Mail Address, Phone Number, and Favorite Color
